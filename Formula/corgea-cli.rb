28ba117ccd18bdb36119d0a520a0719607682fb3
Ibrahimrahhal <Ibrahimrahhal@users.noreply.github.com>
2026-08-12 02:49:00 +0000

    Update corgea-cli to version 1.10.0

--- Formula/corgea-cli.rb
+++ Formula/corgea-cli.rb
@@ -3,8 +3,8 @@ class CorgeaCli < Formula
 
   desc "CLI tool for corgea"
   homepage "https://pypi.org/project/corgea-cli/"
-  url "https://files.pythonhosted.org/packages/c0/57/3972506e200fd929049fe0f34de9d7980dc2c32447d8b99561bab531952b/corgea_cli-1.8.5.tar.gz"
-  sha256 "1c2a5fe55e9afd85d85de749e2ca30ba58284688f89ce035b47216efacf0f909"
+  url "https://files.pythonhosted.org/packages/d9/60/3a914267f925b882e0ee6df7b7eaf260a3039543b38729fbdd5fa4b46797/corgea_cli-1.10.0.tar.gz"
+  sha256 "4d14d3e2b4a9fb754aa461c072fe8612a19c88575cb394616c296e449ce185c4"
 
   depends_on "python@3.11"
   depends_on "rust" => :build
